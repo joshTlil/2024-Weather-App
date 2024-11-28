ba8b7dc1f2251befb597f04d27b352c58e33bef5
JoshuaT <jttsat170@gmail.com>
2024-11-28 13:28:45 -0500

    Getting icon to display

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let weatherApiKey = "0f272bc818897878df309b4b56d1f500"
 let cityName = "Atlanta"
 let link = 'https://api.openweathermap.org/data/2.5/weather?q='+ cityName + '&appid=' + weatherApiKey
 let searchForm = document.querySelector('.searchBar');
+let iconDisplay = document.querySelector('#icon');
+let locationIcon = document.querySelector('weatherIcon')
+let number = 23
 
 // function searchFunction(event){
 //     event.preventDefault();
@@ -12,6 +15,10 @@ let searchForm = document.querySelector('.searchBar');
 // }
 
 // searchForm.addEventListener('submit', searchFunction);
+
+function getElement(id){
+    return document.getElementById(id);
+}
 
 function getApi(link){
     fetch(link)
@@ -25,7 +32,12 @@ function getApi(link){
     .then(function (data){
         console.log(data.weather[0])
         console.log(data.weather[0].description)
-
+        // getElement('icon').attr = data.weather[0].icon;
+        let iconCode = data.weather[0].icon;
+        let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        console.log(number)
+        iconDisplay.setAttribute('src', iconUrl)
+       
     });
 }
 
