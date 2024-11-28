e5853aba76785b1977f7a4545dcb8c203a529561
JoshuaT <jttsat170@gmail.com>
2024-11-28 12:18:51 -0500

    testing api results

---
+++ index.js
@@ -0,0 +1,30 @@
+let weatherApiKey = "0f272bc818897878df309b4b56d1f500"
+let cityName = "Atlanta"
+let link = 'https://api.openweathermap.org/data/2.5/weather?q='+ cityName + '&appid=' + weatherApiKey
+let searchForm = document.querySelector('.searchBar');
+
+// function searchFunction(event){
+//     event.preventDefault();
+//     let cityName = "Atlanta"
+//     let weatherApiKey = "0f272bc818897878df309b4b56d1f500"
+//     let link = 'https://api.openweathermap.org/data/2.5/weather?q='+ cityName + '&appid=' + weatherApiKey
+
+// }
+
+// searchForm.addEventListener('submit', searchFunction);
+
+function getApi(link){
+    fetch(link)
+    .then(function(response){
+        console.log(response.status)
+        if(response.status !== 200){
+            console.log(response.status)
+        }
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data)
+    });
+}
+
+getApi(link)
