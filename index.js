abd622da9a549559dabc787b026e057edcd40200
JoshuaT <jttsat170@gmail.com>
2024-12-17 20:24:28 -0500

    authorizing issues and deprecated versions in use for api, note solve later

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let weatherApiKey = "0f272bc818897878df309b4b56d1f500"
+let uviKey = "1af9a040b41dacae67d64eaabcbafeec"
 let cityName = "Atlanta"
 let link = 'https://api.openweathermap.org/data/2.5/weather?q='+ cityName + '&appid=' + weatherApiKey
 let forecast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + weatherApiKey
@@ -39,7 +40,12 @@ console.log(forecast)
 //TODO: I need to get the coordinates for lat and lon put in the api link so then i am able to grab the uvi from data
 //The api has to be local in order to have access to it
 //set global variables for now in order to grab the data then plug it in and continue the rest later
-let latitude = get
+let lat = 33.749;
+let lon = -84.388;
+// let uviLink = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon=" + lon +"&exclude=hourly,daily&appid=" + weatherApiKey;
+let uviLink = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid="+ uviKey;
+getUV(uviLink)
+// console.log(latitude)
 
 // function searchFunction(event){
 //     event.preventDefault();
@@ -71,8 +77,8 @@ function getApi(link){
         console.log(data.coord.lat)
         let lon = data.coord.lon
         let lat = data.coord.lat
-        let uviLink = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+".44&lon=" + lon +".04&exclude=minutely,hourly,daily,alerts&appid=" + weatherApiKey;
-        getUV(forecast)
+        // let uviLink = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon=" + lon +"&exclude=hourly,daily&appid=" + weatherApiKey;
+        // getUV(uviLink)
         console.log(data.weather[0])
         console.log(data.weather[0].description)
         // getElement('icon').attr = data.weather[0].icon;
@@ -106,6 +112,11 @@ function getUV(link){
         return response.json()
     })
     .then(function(data){
+        // console.log(data.city.coord.lat)
+        // return data.city.coord.lat
+        // console.log(data.coord.lat)
+        //  latitude = data.coord.lat
+        // getLat(lat);
         console.log(data)
     })
 }
@@ -114,9 +125,9 @@ function getLat(lat){
     return lat;
 }
 
-function getLon(lon){
-    return lon
-}
+// function getLon(lon){
+//     return lon
+// }
 
 
 function getForecast (forecast){
